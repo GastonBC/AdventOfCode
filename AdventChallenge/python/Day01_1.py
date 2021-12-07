02dc0880216ea8bf7a07b94dbf757553f7ceb403
GastonWkLaptop <gastonbalparda@gmail.com>
2021-12-07 14:02:26 -0300

    Visualization

--- AdventChallenge/python/Day01_1.py
+++ AdventChallenge/python/Day01_1.py
@@ -14,3 +14,24 @@ for idx, m in enumerate(measurements):
 print(increase_count)
 
 '''1715'''
+
+# visualization
+import matplotlib.pyplot as plt
+sample = Inputs.Day01()
+
+for x in range(len(sample)):
+    if x == 0:
+        continue
+
+    point1 = (x-1, sample[x-1])
+    point2 = (x, sample[x])
+
+    x_values = [point1[0], point2[0]]
+
+    y_values = [point1[1], point2[1]]
+
+
+    plt.plot(x_values, y_values)
+
+plt.show()
+# visualization
