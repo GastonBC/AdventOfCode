b482fe9f996ee7ef664d8e950b6bd41b22eaa6fa
GastonWkLaptop <gastonbalparda@gmail.com>
2021-12-09 21:29:43 -0300

    Day 3

---
+++ AdventChallenge/rust/day03_01/src/main.rs
@@ -0,0 +1,16 @@
+use puzzle_inputs;
+
+fn main() {
+    let puz_input = puzzle_inputs::inputs::day03();
+
+    let mut gamma = String::new();
+    let mut epsilon = String::new();
+
+    for idx in 0..puz_input.len()
+    {
+        let mut count_0 = 0;
+        let mut count_1 = 0;
+
+    }
+
+}
