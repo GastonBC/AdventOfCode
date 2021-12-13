e74ea669362ee1482268ff6ad3549667b2c734d5
Gas <gastonbalparda@gmail.com>
2021-12-12 23:37:50 -0300

    day 2

--- 2020/rust/puzzle_inputs/src/lib.rs
+++ 2020/rust/puzzle_inputs/src/lib.rs
@@ -1,14 +1,21 @@
 pub mod inputs 
 {
+
     use std::fs;
 
     pub fn day01() -> Vec<i32>
     {
-        let path = "../puzzle_inputs/src/day01.txt";
+        let path = "../puzzle_inputs/src/txtfiles/day01.txt";
 
         let data = fs::read_to_string(path).expect("Unable to read file");
         let lines:Vec<i32> = data.lines().map(|x| x.parse::<i32>().unwrap()).collect();
 
         return lines;   
+    }
+
+    pub fn day02()
+    {
+        let path = "../puzzle_inputs/src/txtfiles/day02.txt";
+        
     }
 }
