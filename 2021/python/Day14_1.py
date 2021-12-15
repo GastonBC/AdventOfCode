ca32d62957c20cf20666eb6f5f049b3cba65d287
Gas <gastonbalparda@gmail.com>
2021-12-14 21:30:46 -0300

    day 14

--- 2021/python/Day14_1.py
+++ 2021/python/Day14_1.py
@@ -34,41 +34,44 @@ rules = dict()
 rules_count = dict()
 ch_occ = dict()
 
+input_template = puz_input.splitlines()[0]
+input_rules = puz_input.splitlines()[1:]
+
 # make rules count dict
-for line in puz_input.splitlines():
+for line in input_rules:
     line = line.replace(" ", "")
-    if "->" in line:
-        rule_ = line.split("->")
-        res_ = rule_[1]
-        cond = rule_[0]
+    
+    inp_rule = line.split("->")
+    cond = inp_rule[0]
+    res_ = inp_rule[1]
+    
 
-        # The polymer process division
-        result = ([cond[0]+res_, res_+cond[1]])
-        rules[cond] = result
-        rules_count[cond] = 0
+    # The polymer process division
+    result = ([cond[0]+res_, res_+cond[1]])
+    rules[cond] = result
+    rules_count[cond] = 0
 
-        # Set up characters in the dict
-        ch_occ[cond[0]] = 0
-        ch_occ[cond[1]] = 0
-        ch_occ[res_] = 0
+    # Set up characters in the dict
+    ch_occ[cond[0]] = 0
+    ch_occ[cond[1]] = 0
+    ch_occ[res_] = 0
 
 # Template line, after the dicts initialization
 # to be able to add counters easy
-for line in puz_input.splitlines():
-    line = line.replace(" ", "")
-    if "->" not in line:
-        for i, ch in enumerate(line):
-            if i != len(line)-1:
-                cond = ch+line[i+1]
-                rules_count[cond] += 1
-                
+input_template = input_template.replace(" ", "")
+i=0
+while i < len(input_template)-1:
+    cond = input_template[i]+input_template[i+1]
+    rules_count[cond] += 1
+    i += 1
+            
 steps = 10
 
 for step in range(steps):
 
     rule_count_copy = rules_count.copy()
     for rl in rule_count_copy:
-        
+
         if rule_count_copy[rl] > 0:
             
             res = rules[rl]
@@ -77,28 +80,28 @@ for step in range(steps):
             # the original poly count
             rules_count[res[0]] += rule_count_copy[rl]
             rules_count[res[1]] += rule_count_copy[rl]
+
+            # The original pair gets used
             rules_count[rl] -= rule_count_copy[rl]
+
 
 for rl in rules_count:
     # first char
     ch_occ[rl[0]] += rules_count[rl]
-
     # second 
     ch_occ[rl[1]] += rules_count[rl]
 
 
-# Dumb proper decimal rounding method
-from decimal import localcontext, Decimal, ROUND_HALF_UP
-with localcontext() as ctx:
-    ctx.rounding = ROUND_HALF_UP
+# Round .5 values up
+from math import ceil
 
-    mx = Decimal(max(ch_occ.values())/2)
-    mn = Decimal(min(ch_occ.values())/2)
-    mx = mx.to_integral_value()
-    mn = mn.to_integral_value()
-    
-    print("Answer is:", mx-mn)
+mx = ceil(max(ch_occ.values())/2)
+mn = ceil(min(ch_occ.values())/2)
 
-'''
-Answer is: 2584
-'''
+print("Answer is:", mx-mn)
+
+print("String lenght:", poly_length(rules_count))
+
+# Correct answers
+assert mx-mn == 2584
+assert poly_length(rules_count) == 19457
