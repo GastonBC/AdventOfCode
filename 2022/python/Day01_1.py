d3777d869cd1c681c51dde9590de2469908a5fa4
Gaston <gastonbalparda@gmail.com>
2022-12-07 12:29:07 -0300

    Day 01 2022

--- 2022/python/Day01_1.py
+++ 2022/python/Day01_1.py
@@ -1,7 +1,14 @@
 import Inputs
-import os
 
+elves = Inputs.Day01()
 
-calories = Inputs.Day01()
+most_calories = 0
 
-print(calories)
+for elve in elves:
+    calories = [int(x) for x in elve.split("\n")]
+    if most_calories < sum(calories):
+        most_calories = sum(calories)
+
+print(most_calories)
+
+assert most_calories == 70369
