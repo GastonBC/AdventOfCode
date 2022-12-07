52e18eb070088579dca732f6911bd4aa35acd895
Gaston <gastonbalparda@gmail.com>
2022-12-07 15:41:17 -0300

    better day 4

--- 2022/python/Day04_2.py
+++ 2022/python/Day04_2.py
@@ -18,8 +18,8 @@ for pair in input:
     e1_section_start, e1_section_end = int(elve1.split("-")[0]), int(elve1.split("-")[1])
     e2_section_start, e2_section_end = int(elve2.split("-")[0]), int(elve2.split("-")[1])
     
-    range_e1 = {*range(int(e1_section_start), int(e1_section_end)+1)}
-    range_e2 = {*range(int(e2_section_start), int(e2_section_end)+1)}
+    range_e1 = {*range(e1_section_start, e1_section_end+1)}
+    range_e2 = {*range(e2_section_start, e2_section_end+1)}
 
     # if any item is contained in the other list, then it's overlapping
     if range_e1.intersection(range_e2):
